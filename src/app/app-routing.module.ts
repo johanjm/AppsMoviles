37c17e1ed31436d85929f959a08404c5891fd200
johanjm <66568293+johanjm@users.noreply.github.com>
2022-11-27 23:07:51 -0500

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { canActivate, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/compat/auth-guard';
+
+const redirectUnauthorizedToLogin = () =>
+  redirectUnauthorizedTo(['/']);
+
+const redirectLoggedInToChat = () => redirectLoggedInTo(['/chat']);
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule),
+    ...canActivate(redirectLoggedInToChat),
+  },
+  {
+    path: 'chat',
+    ...canActivate(redirectUnauthorizedToLogin),
+    loadChildren: () => import('./pages/chat/chat.module').then( m => m.ChatPageModule)
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/quotes */
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCucu94RvuNw3GEew_3j9AVkRFtYyXOg5Y",
+    authDomain: "apps2022-d7f9c.firebaseapp.com",
+    projectId: "apps2022-d7f9c",
+    storageBucket: "apps2022-d7f9c.appspot.com",
+    messagingSenderId: "531245062270",
+    appId: "1:531245062270:web:508d470730c04ee952e02f",
+    measurementId: "G-5PBWGS9EJM"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
